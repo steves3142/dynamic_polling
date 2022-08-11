a699de8bc8089771a5fe33d07a73f060ab602a6a
atn95 <antannguyen2000@gmail.com>
2022-08-11 15:11:34 -0500

    some empty pages/component/css module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Student from './pages/Student';
+import Host from './pages/Host';
+import styles from './styles/App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<Routes>
+				<Route path='/' element={<div>Home Page</div>} />
+				<Route path='/student' element={<Student />} />
+				<Route path='/host' element={<Host />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Student.jsx
@@ -0,0 +1,4 @@
+import styles from '../styles/pages/Student.module.css';
+export default function Student() {
+	return <div className={styles.container}>Student</div>;
+}
