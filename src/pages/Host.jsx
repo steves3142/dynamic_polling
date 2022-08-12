a220f0cc9038629285473353d6ffd45a83ef7633
steves3142 <steves3142@gmail.com>
2022-08-12 00:41:04 -0400

    CSS styling for host

--- src/pages/Host.jsx
+++ src/pages/Host.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import styles from '../styles/pages/Host.css'
 
 
 export default function Host() {
+	
+	const [formState, setFormState ] = useState([]) 
 
+	const handleChange = event => {
+		setFormState({ ...formState, [event.target.id]: event.target.value });
+	  };
+	
 
+	return(
 
+	<div className={styles.container}>
+		<h2>Host Display</h2>
+		<div id = "rectangle" className="first">
 
+		
+		<div id ="rectangle" className="second">
+				Write Questions Here
+		</div>
 
-
-	return <div>Host</div>;
+		</div>
+		<div id = "rectangle" className="third">
+				Text Field for Option1	
+		</div> 
+	</div>
+	
+	)
 }
