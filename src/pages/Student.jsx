a7da0f955662b716acb59c6d443cb2b5bbe21405
atn95 <antannguyen2000@gmail.com>
2022-08-11 19:44:43 -0500

    added socket.io-client and successfully connected to backend

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,4 +1,16 @@
-import styles from '../styles/pages/Student.module.css';
+import styles from '../styles/pages/Student.module.css'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3001')
+
 export default function Student() {
-	return <div className={styles.container}>Student</div>;
+	function doSomething() {
+		socket.emit('student-message', { message: 'student' })
+	}
+	return (
+		<div className={styles.container}>
+			Student
+			<button onClick={doSomething}>Test backend msg</button>
+		</div>
+	)
 }
