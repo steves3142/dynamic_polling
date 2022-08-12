34fd269e08159cb702d806eafc290cc04fffb3d6
steves3142 <steves3142@gmail.com>
2022-08-12 15:07:42 -0400

    Revert "Merge branch 'steve'"
    This reverts commit f77370ee04613d50c4c2627573a5e2ada4e00e99, reversing
    changes made to 36311710240e5656adf17a9012f5f0707cd6b15a.

--- src/pages/Host.jsx
+++ src/pages/Host.jsx
@@ -5,25 +5,20 @@ import axios from 'axios'
 
 export default function Host() {
 
-	const initialForm = { 
-		question: '', 
-		room_Id: '10',
-		type: 'MC',
-	  }
-	const [formState, setFormState] = useState(initialForm)
+	const [formState, setFormState] = useState([])
 
 	const handleChange = event => {
 		setFormState({ ...formState, [event.target.id]: event.target.value });
 	};
 
-	async function handleSubmit(event) {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
-		let res = await axios.post(`/host`, formState)
+		let res = await axios.post(``, formState)
 		console.log('hola')
 		console.log(res.data)
 		setFormState([])
-		getBanks()
-	  };
+
+	};
 
 	return (
 		<div>
@@ -33,29 +28,36 @@ export default function Host() {
 				<div id="rectangle" className={styles.options}>
 
 					<div className={styles.wrapper}>
+						<form>
 
 							<div id="rectangle" className={styles.options}>
 								<label htmlFor="question">Type Your Question Here</label>
 								<input type="text" onChange={handleChange} />
 							</div>
 
-						<div id="rectangle" className={styles.options}>
-							Text Field for Option 1
-						</div>
+							<div id="rectangle" className={styles.options}>
+								<label htmlFor="question">Type First Answer Choice</label>
+								<input type="text" onChange={handleChange} />
+							</div>
 
-						<div id="rectangle" className={styles.options}>
-							Text Field for Option 2
-						</div>
+							<div id="rectangle" className={styles.options}>
+								<label htmlFor="question">Type Second Answer Choice</label>
+								<input type="text" onChange={handleChange} />
+							</div>
 
-						<div id="rectangle" className={styles.options}>
-							Text Field for Option 3
-						</div>
+							<div id="rectangle" className={styles.options}>
+								<label htmlFor="question">Type Third Answer Choice</label>
+								<input type="text" onChange={handleChange} />
+							</div>
 
-						<div id="rectangle" className={styles.options}>
-							Text Field for Option 4
-						</div>
+							<div id="rectangle" className={styles.options}>
+								<label htmlFor="question">Type Fourth Answer Choice</label>
+								<input type="text" onChange={handleChange} />
+							</div>
 
-						<button onClick={handleSubmit}>Submit</button> 
+							<button type="submit" onClick={handleSubmit}>Submit</button>
+
+						</form>
 
 					</div>
 
