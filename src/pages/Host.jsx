dfadd1a3976a7e4ec6e70d908a2902d92b6cae7d
steves3142 <steves3142@gmail.com>
2022-08-12 11:39:38 -0400

    Added boxes for options and questions on teacher side form

--- src/pages/Host.jsx
+++ src/pages/Host.jsx
@@ -1,34 +1,52 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import styles from '../styles/pages/Host.css'
+import styles from '../styles/pages/Host.module.css'
 
 
 export default function Host() {
-	
-	const [formState, setFormState ] = useState([]) 
+
+	const [formState, setFormState] = useState([])
 
 	const handleChange = event => {
 		setFormState({ ...formState, [event.target.id]: event.target.value });
-	  };
-	
+	};
 
-	return(
 
-	<div className={styles.container}>
-		<h2>Host Display</h2>
-		<div id = "rectangle" className="first">
+	return (
 
-		
-		<div id ="rectangle" className="second">
-				Write Questions Here
+
+
+		<div className={styles.container}>
+
+			<div id="rectangle" className={styles.options}>
+
+				<div className={styles.wrapper}>
+
+					<div id="rectangle" className={styles.options}>
+						Write Questions Here
+					</div>
+
+					<div id="rectangle" className={styles.options}>
+						Text Field for Option1
+					</div>
+
+					<div id="rectangle" className={styles.options}>
+						Text Field for Option 2
+					</div>
+
+					<div id="rectangle" className={styles.options}>
+						Text Field for Option 3
+					</div>
+
+					<div id="rectangle" className={styles.options}>
+						Text Field for Option 4
+					</div>
+
+				</div>
+
+			</div>
 		</div>
 
-		</div>
-		<div id = "rectangle" className="third">
-				Text Field for Option1	
-		</div> 
-	</div>
-	
 	)
 }
