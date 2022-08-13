65ddc87c8a88a9c9962ae728cf265abcbe816819
atn95 <antannguyen2000@gmail.com>
2022-08-12 22:45:31 -0500

    swapped naming for login and register jsx and some styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,58 +4,30 @@ import styles from '../styles/pages/Login.module.css'
 import axios from 'axios'
 
 export default function Login() {
+	return (
+		<div className={styles.container}>
+			<h2>Login</h2>
+			<div id='rectangle' className={styles.register_box1}>
+				<div id='rectangle' className={styles.register_box2}>
+					Email
+				</div>
 
-    const [formState, setFormState] = useState([])
+				<div id='rectangle' className={styles.register_box3}>
+					Password
+				</div>
 
-    const initialForm = {
-        email: '',
-        passwordDigest: '',
-        type: 'MC',
-    }
+				<div id='rectangle' className={styles.signup_box1}>
+					<button className={styles.button}>Sign In</button>
+				</div>
 
+				<div id='rectangle' className={styles.keep_signedin}>
+					Keep Signed In
+				</div>
 
-	const handleChange = (event) => {
-		setFormState({ ...formState, [event.target.id]: event.target.value })
-	}
-
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        let res = await axios.post(`http://localhost:3001/api/host`, formState)
-        console.log('hola')
-        console.log(res.data)
-        setFormState(initialForm)
-    }
-
-    return (
-        <div>
-            <h2>Login</h2>
-            <div className={styles.container}>
-                <form onSubmit={handleSubmit}>
-                    <div id="rectangle" className={styles.login_box1}>
-                        <div id="rectangle" className={styles.login_box2}>
-                            Email
-                            <label htmlFor="question">Type Your Question Here</label>
-								<input type="text" id="question" onChange={handleChange} value={formState.email} />
-                        </div>
-
-                        <div id="rectangle" className={styles.login_box3}>
-                            Password
-                        </div>
-
-                        <div id="rectangle" className={styles.signup_box1}>
-                            <button onClick={handleSubmit} className={styles.button}>Sign In</button>
-                        </div>
-
-                        <div id="rectangle" className={styles.signup_box2}>
-                            <button className={styles.button}>Sign Up</button>
-                        </div>
-
-                    </div>
-                    
-                </form>
-
-            </div>
-
-        </div >
-    )
+				<div id='rectangle' className={styles.signup_box2}>
+					<button>Sign Up</button>
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,38 +3,60 @@ import styles from '../styles/pages/Register.module.css'
 
 import axios from 'axios'
 
-
 export default function Register() {
+	const [formState, setFormState] = useState([])
 
-    return (
-        <div>
-            <h2>Register</h2>
-            <div className={styles.container}>
+	const initialForm = {
+		email: '',
+		password: '',
+		confirmPassword: '',
+		type: 'MC',
+	}
 
-                <div id="rectangle" className={styles.register_box1}>
-                    <div id="rectangle" className={styles.register_box2}>
-                        Email
-                    </div>
+	const handleChange = (event) => {
+		setFormState({ ...formState, [event.target.id]: event.target.value })
+	}
 
-                    <div id="rectangle" className={styles.register_box3}>
-                        Password
-                    </div>
+	const handleSubmit = async (event) => {
+		event.preventDefault()
+		let res = await axios.post(`http://localhost:3001/api/host`, formState)
+		console.log('hola')
+		console.log(res.data)
+		setFormState(initialForm)
+	}
 
-                    <div id="rectangle" className={styles.signup_box1}>
-                        <button className={styles.button}>Sign In</button>
-                    </div>
-
-                    <div id="rectangle" className={styles.keep_signedin}>
-                        Keep Signed In
-                    </div>
-
-                    <div id="rectangle" className={styles.signup_box2}>
-                    <button>Sign Up</button>
-                    </div>
-
-                </div>
-            </div>
-
-        </div >
-    )
+	return (
+		<div className={styles.container}>
+			<h2>Register</h2>
+			<form onSubmit={handleSubmit}>
+				<div id='rectangle' className={styles['form-wrapper']}>
+					<input
+						className={styles['login-element']}
+						type='email'
+						id='email'
+						onChange={handleChange}
+						value={formState.email}
+						placeholder='email@email.com'
+					/>
+					<input
+						type='password'
+						id='password'
+						onChange={handleChange}
+						value={formState.password}
+						placeholder='Password'
+						className={styles['login-element']}
+					/>
+					<input
+						type='password'
+						id='confirmPassword'
+						onChange={handleChange}
+						value={formState.password}
+						placeholder='Confirm Password'
+						className={styles['login-element']}
+					/>
+					<div className={styles['pseudo-button']}>Sign Up</div>
+				</div>
+			</form>
+		</div>
+	)
 }
