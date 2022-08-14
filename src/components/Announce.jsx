4489a190d7183ff49b7404ecb0c62c4caf44da22
steves3142 <steves3142@gmail.com>
2022-08-14 16:54:26 -0400

    Can type into room announcement box now

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import styles from '../styles/components/Announce.module.css'
 
 export default function Announcement() {
 
     // const [announce, setAnnounce] = useState([])
-    // let [input, setInput] = useState('')
+    let [input, setInput] = useState('')
 
 
-	// function handleChange(e) {
-	// 	setInput(e.target.value)
-	// }
-    
-    // function handleSubmit(e) {
-	// 	e.preventDefault()
-	// 	socket.emit('send-message', { message: input })
-	// 	setInput('')
-	// }
+    function handleChange(e) {
+    	setInput(e.target.value)
+    }
+
+    function handleSubmit(e) {
+    	e.preventDefault()
+    	// socket.emit('send-message', { message: input })
+    	setInput('')
+    }
 
     return (
+
         <div className={styles['pseudo-button']}>
             Room Announcement
+
+            <div className={styles.container}>
+                <form onSubmit={handleSubmit} className={styles['form-wrapper']}>
+                    <input
+                        className={styles.form}
+                        type='text'
+                        name='chatInput'
+                        onChange={handleChange}
+                        value={input}
+                    />
+                    <button className={styles.button} type='submit'>
+                        SEND
+                    </button>
+                </form>
+            </div>
+
+
         </div>
-// </div>
-// <form onSubmit={handleSubmit} className={styles['form-wrapper']}>
-//     <input
-//         className={styles.form}
-//         type='text'
-//         name='chatInput'
-//         onChange={handleChange}
-//         value={input}
-//     />
-//     <button className={styles.button} type='submit'>
-//         SEND
-//     </button>
-// </form>
-// </div>
-        
     )
 }
