36798835bffe7724f987caa9249bb483db21d3cf
steves3142 <steves3142@gmail.com>
2022-08-14 18:51:50 -0400

    Announce

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import styles from '../styles/components/Announce.module.css'
 
+// export default function Announcement({ socket }) {
 export default function Announcement() {
 
     // const [announce, setAnnounce] = useState([])
-    // let [input, setInput] = useState('')
+    let [input, setInput] = useState('')
 
 
-	// function handleChange(e) {
-	// 	setInput(e.target.value)
-	// }
-    
-    // function handleSubmit(e) {
-	// 	e.preventDefault()
-	// 	socket.emit('send-message', { message: input })
-	// 	setInput('')
-	// }
+    function handleChange(e) {
+        setInput(e.target.value)
+    }
+
+    function handleSubmit(e) {
+		e.preventDefault()
+		// socket.emit('room-announce', { message: input })
+		setInput('')
+    }
 
     return (
-        <div className={styles['pseudo-button']}>
-            Room Announcement
+
+
+        <div>
+            <div className={styles['button-container']}>
+                <div className={styles['pseudo-button']}>
+                    Room Announcement
+                </div>
+            </div>
+
+            <div>
+                <div className={styles['container']}>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            className={styles.form}
+                            type='text'
+                            name='chatInput'
+                            size='60'
+                            width='60'
+                            height='60'
+                            onChange={handleChange}
+                            value={input}
+                        />
+                        <div>
+                            <button className={styles.button} type='submit'>
+                                SEND
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-// </div>
-// <form onSubmit={handleSubmit} className={styles['form-wrapper']}>
-//     <input
-//         className={styles.form}
-//         type='text'
-//         name='chatInput'
-//         onChange={handleChange}
-//         value={input}
-//     />
-//     <button className={styles.button} type='submit'>
-//         SEND
-//     </button>
-// </form>
-// </div>
-        
     )
 }
