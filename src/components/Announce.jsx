a0c132fcd21db45aa18bd1df17b60b8aabc48093
atn95 <antannguyen2000@gmail.com>
2022-08-14 19:30:29 -0500

    socket message for announce

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -1,13 +1,9 @@
 import { useState, useEffect } from 'react'
 import styles from '../styles/components/Announce.module.css'
 
-
 export default function Announcement({ socket }) {
-
-
 	// const [announce, setAnnounce] = useState([])
 	let [input, setInput] = useState('')
-
 
 	function handleChange(e) {
 		setInput(e.target.value)
@@ -15,22 +11,16 @@ export default function Announcement({ socket }) {
 
 	function handleSubmit(e) {
 		e.preventDefault()
-		// socket.emit('room-announce', { message: input })
+		socket.emit('room-announce', { message: input })
 		setInput('')
 	}
 
 	return (
-
-
 		<div className={styles['master-container']}>
-			<div className={styles['pseudo-button']}>
-
-					Room Announcement
-
-			</div>
+			<div className={styles['pseudo-button']}>Room Announcement</div>
 
 			<div>
-			<div className={styles['message-container']}>
+				<div className={styles['message-container']}>
 					<div className={styles['container']}>
 						<form onSubmit={handleSubmit}>
 							<input
@@ -44,15 +34,16 @@ export default function Announcement({ socket }) {
 								value={input}
 							/>
 							<div>
-								<button className={styles.button} type='submit'>
+								<button
+									onClick={handleSubmit}
+									className={styles.button}
+									type='submit'>
 									SEND
 								</button>
 							</div>
 						</form>
 					</div>
-
 				</div>
-
 			</div>
 		</div>
 	)
