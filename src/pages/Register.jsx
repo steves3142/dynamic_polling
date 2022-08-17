c04ca16ca17b51f915b30224a84930225d209702
steves3142 <steves3142@gmail.com>
2022-08-17 13:57:06 -0400

    Updated Register with Display Name

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,6 +10,7 @@ export default function Register() {
 		email: '',
 		password: '',
 		confirmPassword: '',
+		displayName:'',
 		type: 'CLIENT',
 	}
 	const [formState, setFormState] = useState(initialForm)
@@ -67,6 +68,15 @@ export default function Register() {
 						onChange={handleChange}
 						value={formState.email}
 						placeholder='email@email.com'
+						required
+					/>
+					<input
+						className={styles['login-element']}
+						type='displayname'
+						id='displayname'
+						onChange={handleChange}
+						value={formState.displayName}
+						placeholder='User Name'
 						required
 					/>
 					<input
